ed77c10ce1fd4a7d6dbcc48859b3f2be496d1edc
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2024-05-16 09:53:22 +0200

    cleanup project structure

--- scripts/ecs/System.gd
+++ scripts/ecs/System.gd
@@ -6,23 +6,23 @@ class_name System
 @export var time_per_tick: float = 1.0 / 60.0
 
 func _ready() -> void:
-	## Do not start a timer if this is 0 or lower.
-	if time_per_tick <= 0:
-		return
-		
-	var timer := Timer.new()
-	add_child(timer)
-	timer.process_callback = Timer.TIMER_PROCESS_PHYSICS
-	timer.timeout.connect(tick)
-	timer.start(time_per_tick)
+    ## Do not start a timer if this is 0 or lower.
+    if time_per_tick <= 0:
+        return
+        
+    var timer := Timer.new()
+    add_child(timer)
+    timer.process_callback = Timer.TIMER_PROCESS_PHYSICS
+    timer.timeout.connect(tick)
+    timer.start(time_per_tick)
 
 func tick():
-	_tick()
+    _tick()
 
 func _tick():
-	pass
+    pass
 
 func debug_msg(variant: Variant):
-	if not debug_mode:
-		return
-	print_debug(variant)
+    if not debug_mode:
+        return
+    print_debug(variant)
