f9ec2721a7e167cb4be4708fd644394679dc3d72
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2024-05-16 22:14:46 +0200

    cleanup code and project structure

--- scripts/ecs/components/ClientIdentifierComponent.gd
+++ scripts/ecs/components/ClientIdentifierComponent.gd
@@ -6,20 +6,20 @@ static var client_entity: Node = null
 @export var client_id: int
 
 func _ready():
-	if client_entity == null:
-		set_own_entity_as_client()
+    if client_entity == null:
+        set_own_entity_as_client()
 
 func set_client_id(multiplayer_id: int):
-	client_id = multiplayer_id
+    client_id = multiplayer_id
 
 func get_client_id() -> int:
-	return client_id
+    return client_id
 
 func set_own_entity_as_client():
-	ComponentClientIdentifier.set_client_entity(get_parent())
+    ComponentClientIdentifier.set_client_entity(get_parent())
 
 static func set_client_entity(entity: Node):
-	ComponentClientIdentifier.client_entity = entity
+    ComponentClientIdentifier.client_entity = entity
 
 static func get_client_entity() -> Node:
-	return ComponentClientIdentifier.client_entity
+    return ComponentClientIdentifier.client_entity
