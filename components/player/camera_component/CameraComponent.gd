23b53080d6c1fffba5e36f0c81c206230eba94b9
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2024-05-20 21:05:51 +0200

    capture mouse

--- components/player/camera_component/CameraComponent.gd
+++ components/player/camera_component/CameraComponent.gd
@@ -6,34 +6,44 @@ extends Component
 @export var camera_upper_limit: float = 89
 @export var camera_lower_limit: float = -50
 
-var _mouse_captured: bool = true
+var _last_mouse_position: Vector2 = Vector2.ZERO
+var _left_mouse_pressed: bool = true
+var _right_mouse_pressed: bool = false
 
 
 func _input(event):
-	if event.is_action_pressed("ui_cancel"):
-		_mouse_captured = !_mouse_captured
-		if _mouse_captured:
-			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-		else:
-			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if event.is_action_pressed("left_click"):
+		_left_mouse_pressed = true
+	elif event.is_action_released("left_click"):
+		_left_mouse_pressed = false
+	elif event.is_action_pressed("right_click"):
+		# Hide the mouse cursor.
+		_last_mouse_position = event.position
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		_right_mouse_pressed = true
+	elif event.is_action_released("right_click"):
+		# Set the mouse at the last position.
+		# Show the mouse cursor.
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		Input.warp_mouse(_last_mouse_position)
+		_right_mouse_pressed = false
 
-	if event is InputEventMouseMotion:
-		# Rotate the player around the axis.
-		actor.rotate_y(deg_to_rad(-event.relative.x * mouse_sensitivity))
+	elif event is InputEventMouseMotion:
+		if _right_mouse_pressed:
+			# Rotate the player around the axis.
+			actor.rotate_y(deg_to_rad(-event.relative.x * mouse_sensitivity))
 
-		# Look up and down.
-		camera_pivot_point.rotate_x(deg_to_rad(-event.relative.y * mouse_sensitivity))
+			# Look up and down.
+			camera_pivot_point.rotate_x(deg_to_rad(-event.relative.y * mouse_sensitivity))
 
-		# Ensure not to look too far.
-		camera_pivot_point.rotation.x = clamp(
-			camera_pivot_point.rotation.x,
-			deg_to_rad(camera_lower_limit),
-			deg_to_rad(camera_upper_limit)
-		)
-		camera_pivot_point.rotation.z = 0
+			# Ensure not to look too far.
+			camera_pivot_point.rotation.x = clamp(
+				camera_pivot_point.rotation.x,
+				deg_to_rad(camera_lower_limit),
+				deg_to_rad(camera_upper_limit)
+			)
+			camera_pivot_point.rotation.z = 0
 
 
 func _ready():
 	register_component()
-
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
