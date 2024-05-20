c255c799ac63bd7e2ad686ae3170902b296d6f72
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2024-05-20 22:27:30 +0200

    add items class

--- classes/Item.gd
+++ classes/Item.gd
@@ -1,20 +1,29 @@
+@tool
 class_name Item
-extends StaticBody3D
+extends Node3D
 
 enum TYPE { MICS, QUEST, EQUIPMENT, CONSUMABLE, CURRENCY }
 
+## the unique identifier of the item
 @export var uuid: String = "":
 	set(new_uuid):
 		uuid = new_uuid
 		name = new_uuid
 
-var type: TYPE = TYPE.MICS
+## The name of the class of the item
+@export var item_class: String = ""
 
-var item_class: String = ""
+## The type of the item
+@export var type: TYPE = TYPE.MICS
+
+## The area where the item can be looted
+@export var loot_area: Area3D = null
 
 
-func _init():
-	# Disable physics by default
-	collision_layer = 0
+func _get_configuration_warnings() -> PackedStringArray:
+	var warnings: PackedStringArray = []
 
-	collision_mask = 0
+	if loot_area == null:
+		warnings.append("loot area is null, please link a loot area")
+
+	return warnings
