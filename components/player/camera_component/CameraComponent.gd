126974e0a9bc41bbaad75ff021eeaaa77233d746
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2024-05-21 11:48:53 +0200

    add mushroomdog enemy

--- components/player/camera_component/CameraComponent.gd
+++ components/player/camera_component/CameraComponent.gd
@@ -9,6 +9,7 @@ extends Component
 var _last_mouse_position: Vector2 = Vector2.ZERO
 var _left_mouse_pressed: bool = true
 var _right_mouse_pressed: bool = false
+var _picture_mode_enabled: bool = false
 
 
 func _input(event):
@@ -28,9 +29,35 @@ func _input(event):
 		# Set the mouse at the last position.
 		Input.warp_mouse(_last_mouse_position)
 		_right_mouse_pressed = false
+	elif event.is_action_pressed("picture_mode"):
+		_picture_mode_enabled = not _picture_mode_enabled
+		if not _picture_mode_enabled:
+			camera_pivot_point.rotation.y = 0
+
+			# Show the mouse cursor.
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+			# Set the mouse at the last position.
+			Input.warp_mouse(_last_mouse_position)
+		else:
+			# Hide the mouse cursor.
+			_last_mouse_position = actor.get_viewport().get_mouse_position()
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 	elif event is InputEventMouseMotion:
-		if _right_mouse_pressed:
+		if _picture_mode_enabled:
+			# Rotate the player around the axis.
+			camera_pivot_point.rotate_y(deg_to_rad(-event.relative.x * mouse_sensitivity))
+
+			# Look up and down.
+			camera_pivot_point.rotate_x(deg_to_rad(-event.relative.y * mouse_sensitivity))
+
+			# Ensure not to look too far.
+			camera_pivot_point.rotation.x = clamp(
+				camera_pivot_point.rotation.x, deg_to_rad(-89), deg_to_rad(89)
+			)
+			camera_pivot_point.rotation.z = 0
+		elif _right_mouse_pressed:
 			# Rotate the player around the axis.
 			actor.rotate_y(deg_to_rad(-event.relative.x * mouse_sensitivity))
 
