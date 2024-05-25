eb32752abbb7f4921b41060729689321dfa7497c
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2024-05-25 22:02:59 +0200

    move attack to attackcomponent

--- components/common/animation_tree_component/AnimationTreeComponent.gd
+++ components/common/animation_tree_component/AnimationTreeComponent.gd
@@ -5,6 +5,7 @@ extends Component
 
 var _animation_tree: AnimationTree = null
 var _movement_component: MovementComponent = null
+var _attack_component: AttackComponent = null
 
 
 func _ready():
@@ -12,23 +13,36 @@ func _ready():
 
 	_animation_tree = model.get_node("AnimationTree")
 	_movement_component = get_node("../MovementComponent")
+	_attack_component = get_node("../AttackComponent")
+	_attack_component.attacking.connect(_on_attacking)
 
 
 func _physics_process(_delta: float):
 	_animation_tree.set(
-		"parameters/conditions/walking", not _movement_component.input_direction.is_zero_approx() and _movement_component.walking
+		"parameters/conditions/walking",
+		not _movement_component.input_direction.is_zero_approx() and _movement_component.walking
 	)
+
 	_animation_tree.set(
-		"parameters/conditions/running", not _movement_component.input_direction.is_zero_approx() and not _movement_component.walking
+		"parameters/conditions/running",
+		not _movement_component.input_direction.is_zero_approx() and not _movement_component.walking
 	)
+
 	_animation_tree.set(
 		"parameters/conditions/idle", _movement_component.input_direction.is_zero_approx()
 	)
+
 	_animation_tree.set(
 		"parameters/WalkBlendSpace2D/blend_position",
 		Vector2(_movement_component.input_direction.x, _movement_component.input_direction.y)
 	)
+
 	_animation_tree.set(
 		"parameters/RunBlendSpace2D/blend_position",
 		Vector2(_movement_component.input_direction.x, _movement_component.input_direction.y)
 	)
+
+
+func _on_attacking():
+	var playback = _animation_tree.get("parameters/playback")  # "locomotion" is my AnimationNodeStateMachine
+	playback.start("Slash")
